5615bc85f50bb8ed5cbf9bd874dbd7dc9cec0d9e
Danh Vo <danh.vo.2110@gmail.com>
2025-03-01 00:45:32 +0700

    signup frontend2

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import { useState } from "react";
+import { useAuthStore } from "../store/useAuthStore";
 
 const SignUpPage = () => {
+
+  const { showPassword, setShowPassword } = useState(false)
+  const { formData, setFormData } = useState({
+    fullName: "",
+    email: "",
+    password: ""
+  })
+
+  const { isSignUp, signUp } = useAuthStore()
   return <div>SignUpPage</div>;
 };
 
