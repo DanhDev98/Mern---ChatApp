d5327faaf03ba49b180c344f488ca3542bd15ffe
Danh Vo <danh.vo.2110@gmail.com>
2025-03-07 18:52:21 +0700

    update last time

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,10 +19,10 @@ app.use(
 );
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/dist')))
-    app.get('*', (req, res) => res.sendFile
-    (path.resolve(__dirname, 'frontend', 'dist', 'index.html')))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+  app.get('*', (req, res) => res.sendFile
+    (path.resolve(__dirname, '../frontend', 'dist', 'index.html')))
 }
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
