e27b586a85c425ef4517bce797ab1cc9d0d6917e
Danh Vo <danh.vo.2110@gmail.com>
2025-03-07 21:36:05 +0700

    update last time DONE

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,16 +11,16 @@ dotenv.config();
 app.use(express.json({ limit: "50mb" })); // Giới hạn 50MB
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
   })
 );
-const PORT = process.env.PORT;
-const __dirname = path.resolve();
-app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')))
   app.get('*', (req, res) => res.sendFile
