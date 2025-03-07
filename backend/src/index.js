89d7acb993253b0bedd997778874ba6186071af5
Danh Vo <danh.vo.2110@gmail.com>
2025-03-07 19:07:21 +0700

    complete lasttime

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,11 +19,11 @@ app.use(
 );
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist')))
   app.get('*', (req, res) => res.sendFile
-    (path.resolve(__dirname, '../frontend', 'dist', 'index.html')))
+    (path.join(__dirname, '../frontend', 'dist', 'index.html')))
 }
-app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
 server.listen(PORT, connectDB());
